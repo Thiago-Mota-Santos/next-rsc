ebc664fb8c2e7259244c7eb0312ab58c88341cdf
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-02 17:05:33 -0300

    feat(relay) add environment

---
+++ src/app/getItem.ts
@@ -0,0 +1,8 @@
+async function getItem (){
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    return res.json()
+}
+
+
+export { getItem }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { getItem } from "./getItem";
 
-export default function Home() {
+export default async function Home() {
+
+  const item = await getItem() // cache MISS
+  console.log(item)
+
   return (
   <div className="h-screen flex items-center justify-center">
      <h1>hello world</h1>
